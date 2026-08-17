da198d1c0620c316f7f8974f78232195adc3340e
Samuel Obunga <158032461+Obunga4@users.noreply.github.com>
2026-08-17 23:01:08 +0300

    Question_C.vb

--- Question_C.vb
+++ Question_C.vb
@@ -1,3 +1,19 @@
 ' Question C
-' VB.NET source file
-' Add the solution code for this question here.
+Imports System.IO 
+ 
+Dim filePath As String = "data.txt" 
+ 
+Using reader As New StreamReader(filePath) 
+    Dim contents As String = reader.ReadToEnd() 
+    MessageBox.Show(contents) 
+End Using 
+
+'  Simple Explanation
+
+This code opens a text file called `data.txt, reads all the information inside it, and then displays the contents in a message box.
+
+* `StreamReader` is used to read the file.
+* `ReadToEnd()` reads everything in the file.
+* `MessageBox.Show()` displays the information.
+* `Using` automatically closes the file after reading it.
+
