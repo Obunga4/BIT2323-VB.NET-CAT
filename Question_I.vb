35ae36320898b54fb2a1e701b5704a06fd62ba20
Samuel Obunga <158032461+Obunga4@users.noreply.github.com>
2026-08-17 23:05:12 +0300

    Question_I.vb

--- Question_I.vb
+++ Question_I.vb
@@ -1,3 +1,22 @@
 ' Question I
-' VB.NET source file
-' Add the solution code for this question here.
+Public Class Form1 
+    Private WithEvents btnShowTime As New Button() 
+    Private lblDateTime As New Label() 
+ 
+    Private Sub btnShowTime_Click(sender As Object, e As EventArgs) _ 
+        Handles btnShowTime.Click 
+ 
+        lblDateTime.Text = DateTime.Now.ToString() 
+    End Sub 
+End Class 
+
+Explanation
+
+This code creates a button and a label on a Windows Form. 
+When the user clicks the Show Time button, the program gets the current date and time and displays it on the label.
+
+* `btnShowTime` = the button.
+* `lblDateTime` = the label.
+* `DateTime.Now` = gets the current date and time.
+* `lblDateTime.Text` = displays the date and time.
+
